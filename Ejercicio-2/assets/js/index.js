190228c16a412777efb4ac9ed1083f7cb2d5ef68
DannygonGE <dannyesdv@gmail.com>
2022-10-14 13:03:51 -0500

    Visual

---
+++ Ejercicio-2/assets/js/index.js
@@ -0,0 +1,24 @@
+var typed = new Typed ('.typed', {
+    strings : [
+        'Daniel'
+    ],
+    typeSpeed: 200,
+    backSpeed: 0,
+    loop: true,
+    loopCount: false
+})
+
+var typed = new Typed ('.typed2', {
+    strings : [
+        'Daniel, Maria'
+    ],
+    typeSpeed: 150,
+    backSpeed: 0,
+    loop: true,
+    loopCount: false
+})
+
+// API //
+
+const Api_Name = 'https://api.agify.io?name=michael';
+const Api_Loc = 'https://api.agify.io?name=michael&country_id=US';
