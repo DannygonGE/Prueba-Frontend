b8c1c6c67317f2115ecf667d16a1434a297cbf39
DannygonGE <dannyesdv@gmail.com>
2022-10-15 10:54:29 -0500

    Javascript

--- Ejercicio-2/assets/js/index.js
+++ Ejercicio-2/assets/js/index.js
@@ -12,7 +12,7 @@ var typed = new Typed ('.typed', {
 
 var typed = new Typed ('.typed2', {
     strings : [
-        'Daniel, Maria'
+        'Daniel Maria'
     ],
     typeSpeed: 150,
     backSpeed: 0,
@@ -58,54 +58,82 @@ inputs.forEach((input) => {
     input.addEventListener('blur', ValidacionFormulario);
 })
 
-
-
 ////////////////////////////////////////////////////
 
-let nombre = document.getElementById('nameInput');
+//let nombre = document.getElementById('nameInput');
 
-button.addEventListener('click', PredecirNombre)
+// button.addEventListener('click', PredecirNombre)
 
-function PredecirNombre() {
-    let info = document.getElementById('nameInput').value
+// function PredecirNombre() {
+//     let info = document.getElementById('nameInput').value
 
-    let loc = document.getElementById('loc').value
+//     let loc = document.getElementById('loc').value
 
-    let array = info.split(',');
+//     let array = info.split(',');
 
-    console.log(array);
-    console.log(loc);
-}
+//     console.log(array);
+//     console.log(loc);
+// }
 
-const api = () => {
-    fetch('https://api.agify.io?name='+name,{ method:'GET'})
-    .then(function(response){return response.json();})
-}
-
-
+// const api = () => {
+//     fetch('https://api.agify.io?name='+name,{ method:'GET'})
+//     .then(function(response){return response.json();})
+// }
 
 
-let output = document.getElementById('output')
-document.getElementById('button').addEventListener('click',predictName);
-let name = document.getElementById('nameInput');
-document.getElementById('nameInput').addEventListener('change',checkName)
+
+let output = document.getElementById('output');
+button.addEventListener('click',predictName);
+let nameInput = document.getElementById('nameInput');
+nameInput.addEventListener('change',checkName);
+let loc = document.getElementById('loc');
+
 function predictName(){
-         let name = nameInput.value;
+        
+        if (output.hasChildNodes()) {
+            output.childNodes.forEach(child => output.removeChild(child));
+        }
+        
+        output.innerHTML = 'Procesando, espera un momento ...';
+        output.style.display = 'block'; 
+        let url = 'https://api.agify.io?'
+        let names = nameInput.value.split(" ")
+        let total_names = names.length;
 
-         fetch('https://api.agify.io?name='+name,{ method:'GET'})
+        let names_params = ""
+        for (var name_idx = 0; name_idx < total_names; name_idx++) {
+            names_params = names_params + "name[]=" + names[name_idx] + "&"
+        }
+
+        let localization = loc.value == "" ? "US" : loc.value
+        url = url + names_params + "country_id=" + localization
+        console.log(url)
+
+         fetch(url,{ method:'GET'})
          .then(function(response){return response.json();})
          .then(data => {
-             if (name != '') {
-                output.innerHTML = "I guess &#129300 your age is " + data.age;
-                output.style.display = 'block';
-                 console.log(data.age);
-             }else{
-                output.style.display = 'none'; 
-             }
+            //  if (name != '') {
+                console.log(data);
+
+                for (var result_idx = 0; result_idx < data.length; result_idx++) {
+
+                    //output.innerHTML = "I guess &#129300 your age is " + data.age;
+                    //output.style.display = 'block';
+
+                    var para = document.createElement('p');
+                    para.textContent = "For " +data[result_idx].name + " in " + data[result_idx].country_id +   "I guess your age is " + data[result_idx].age;
+                    output.after(para);
+                }
+            //  }else{
+            //     output.style.display = 'none'; 
+            //  }
 
 
          })
-         .catch(err => console.log(err));
+         .catch(err => {
+            output.innerHTML = "Opss, :'( algo ha sucedido..." + err;
+            console.log(err)
+         });
      }
 
  function checkName(){
